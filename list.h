fdf52148e85d189ec7726d365e2a847ded72c1ed
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-03-17 14:43:55 +0530

    bug fixes

--- list.h
+++ list.h
@@ -4,8 +4,8 @@ struct list
 	struct list *next;
 };
 
-(struct list)* add_rear(struct list *head,const void *data);
+struct list* add_rear(struct list *head,const void *data);
 
-(struct list)* del_rear(struct list *head);
+struct list* del_rear(struct list *head);
 
-(struct list)* display(struct list *head);
+struct list* display(struct list *head);
