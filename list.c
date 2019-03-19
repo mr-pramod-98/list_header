d641fd16135a259b921bf11895dddc7b9737a65c
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-03-19 09:11:22 +0530

    added display function

--- list.c
+++ list.c
@@ -26,17 +26,22 @@ struct list* add_rear(struct list *head,const void *data)
 	return head;	
 }
 
-struct list* del_rear(struct list *head)
+struct list* display(struct list *head)
 {
-	struct list *temp1,*temp2;
-	temp1 = head;
-	temp2 = NULL;
-	while(temp1->next == NULL)
-	{
-		temp2 = temp1
-		temp1 = temp1->next;
-	}
-	temp2->next = NULL;
-	free(temp1);
-	return head;
+     struct list *temp;
+     int data;	
+     temp = head;
+     if(head->next == NULL)
+     {
+         return NULL;
+     }
+     else
+     {
+         while(temp == NULL)
+         {
+	     data = (int)temp->data;
+             printf("%d\n",data);
+             temp = temp->next;
+         }	
+     }
 }
