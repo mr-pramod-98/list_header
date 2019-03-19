81fd25e7a1fd47c3be8cc1e9b5ea93f67681e201
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-03-19 09:09:43 +0530

    removed structure def and display function

--- main.c
+++ main.c
@@ -1,31 +1,11 @@
 #include<stdio.h>
-struct list
-{
-	int data;
-	struct list *next;
-}; 
+#include"list.h"
 
-struct list* display(struct list *head)
-{
-     struct list *temp;
-     temp = head;
-     if(head->next == NULL)
-     {
-         return NULL;
-     }
-     else
-     {
-         while(temp == NULL)
-         {
-             printf("%d\n",temp->data);
-             temp = temp->next;
-         }	
-     }
-}
 
 void main()
 {
  int data[3] = {12,34,5};
+ int i;
  struct list * head = NULL; 
  for(i = 0;i < 3; i++)
        head = add_rear(head,data[i]);
